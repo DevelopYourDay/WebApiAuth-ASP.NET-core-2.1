6c8bcc7f525a4f755916035711f7b2fefc7e08e3
Ricardo Santos <1151148@isep.ipp.pt>
2018-10-30 15:10:40 +0000

    AuthWebAPi ASP.NET core 2.1 + LocalDB

---
+++ WebApiAuth/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebApiAuth.Dtos;
+using WebApiAuth.Entity;
+
+
+namespace WebApiAuth.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
